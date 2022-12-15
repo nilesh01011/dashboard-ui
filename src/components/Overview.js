2efc42c99000d7f078a84c117b68ae1ee9c6bb64
Nilesh Rathod <79071386+nilesh01011@users.noreply.github.com>
2022-12-15 20:49:59 +0530

    Update Overview.js

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import { divisionsData, fullYearsOverview, plantsData, regionsData } from '../Data'
+import OverviewBoxs from './OverviewBoxs'
+import SelectBox from './SelectBox'
 
 function Overview() {
     return (
         <div className='mt-8'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <div className='flex flex-col'>
+                {/* title and selectboxs */}
+                <div className='flex items-center justify-between mb-6'>
+                    {/* titles */}
+                    <div className='flex items-center gap-2'>
+                        <img className='h-[28px]' src='./images/Dashboard.svg' alt='Union-images' />
+                        <h1 className='font-bold sm:text-2xl text-lg'>Dashboard</h1>
+                    </div>
+
+                    {/* selectBox */}
+                    <div className='flex gap-[20px]'>
+                        <SelectBox items={fullYearsOverview} />
+                        <SelectBox items={divisionsData} />
+                        <SelectBox items={plantsData} />
+                        <SelectBox items={regionsData} />
+                    </div>
+
+                </div>
+                {/* overview Boxs */}
+                <div className='flex gap-[20px] mb-6'>
+                    <OverviewBoxs title="Total Carbon Footprint" paras="(Scope 1 & Scope 2)" />
+                    <OverviewBoxs title="Intensity Metrics" paras="(Scope 1 & Scope 2)" />
+                    <OverviewBoxs title='Total Carbon in Energy' />
+                </div>
+            </div>
         </div>
     )
 }
