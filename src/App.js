85f99b16a9a2784373a63610ea1790da3ddaf23e
Nilesh Rathod <79071386+nilesh01011@users.noreply.github.com>
2022-12-16 11:16:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import Header from "./components/Header";
 import Tabs from "./components/Tabs";
 
 function App() {
+  let theme = localStorage.getItem("theme");
+
+  if (theme === "light") {
+    document.querySelector("body").style.cssText = 'background:#f4f4f4; color:#000;';
+  } else {
+    document.querySelector("body").style.cssText = 'background:#0B0B0C; color:#fff;';
+  }
+
   return (
     <>
       <Header />
